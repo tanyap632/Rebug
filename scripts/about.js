368c6ff016a3958289dd30c25fcad631e73d1352
Tanya Poh <tanyap632@gmail.com>
2025-07-17 22:41:12 +0500

    js

--- scripts/about.js
+++ scripts/about.js
@@ -1,13 +1,13 @@
-const aboutOpen = document.getElementById('about--button_open');
-const aboutBack = document.getElementById('about--button_back');
-const aboutText = document.getElementById('about--text');
-aboutOpen.addEventListener('click', function(){
-    aboutText.style.display = "block"
-    aboutOpen.style.display = "none"
-    aboutBack.style.display = "flex"
-}) 
-aboutBack.addEventListener('click', function(){
-    aboutText.style.display = "none"
-    aboutOpen.style.display = "flex"
-    aboutBack.style.display = "none"
-})
+const aboutOpen = document.getElementById("about--button_open");
+const aboutBack = document.getElementById("about--button_back");
+const aboutText = document.getElementById("about--text");
+aboutOpen.addEventListener("click", function () {
+  aboutText.style.display = "block";
+  aboutOpen.style.display = "none";
+  aboutBack.style.display = "flex";
+});
+aboutBack.addEventListener("click", function () {
+  aboutText.style.display = "none";
+  aboutOpen.style.display = "flex";
+  aboutBack.style.display = "none";
+});
